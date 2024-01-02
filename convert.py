590cd1f814f80250b9ada9410babc6478d0f5d99
Lev1s <lev1sstudio.cn@gmail.com>
2024-01-02 19:54:46 +0800

    actions/upload-artifact@v2

--- convert.py
+++ convert.py
@@ -14,19 +14,18 @@ def convert_conf_to_txt(conf_file, txt_file, dns_url):
 
 def main():
     current_directory = os.getcwd()  # 获取当前目录
-    # 使用glob匹配以.conf结尾的文件
+    converted_directory = os.path.join(current_directory, 'converted')  # 创建 converted 文件夹
+    os.makedirs(converted_directory, exist_ok=True)  # 确保 converted 文件夹存在
+    # 使用 glob 匹配以 .conf 结尾的文件
     conf_files = glob.glob(os.path.join(current_directory, '*china.conf'))
     # 逐个读取文件内容
     for thefile in conf_files:
         if os.path.basename(thefile) == 'bogus-nxdomain.china.conf':
             continue
-        convert_conf_to_txt(thefile, thefile + ".txt", dns_url)
+        txt_file = os.path.join(converted_directory, os.path.basename(thefile) + ".txt")  # 生成的 txt 文件路径
+        convert_conf_to_txt(thefile, txt_file, dns_url)
 
 # 从环境变量中获取 DNS URL
 dns_url = os.environ.get('DNS_URL')
 
-# 检查 DNS URL 是否为空
-if dns_url is None:
-    print("DNS_URL 环境变量未设置")
-else:
-    main()
+main()
